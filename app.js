804b5ade40c5656ad8433dccf9aca943cc066532
zer0cheros <79189529+zer0cheros@users.noreply.github.com>
2021-09-12 17:13:11 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuClick = document.querySelector('.nav')
+const show = document.querySelector('.showcase')
+
+function menu(){
+    show.classList.toggle('active')
+    menuClick.classList.toggle('active')
+}
